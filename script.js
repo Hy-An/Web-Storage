ac56ff3adb9a3451c4c51e4c970874d664c2dc97
Exequielt <exequield.torres@gmail.com>
2021-06-02 15:13:48 -0300

    small fixes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let TEXTBOX = document.getElementById("texto");
+let TEXT_BOX = document.getElementById("texto");
 let BUTTON = document.getElementById("boton");
 
 BUTTON.addEventListener("click", function () {
-  localStorage.name = TEXTBOX.value;
-  TEXTBOX.value = "";
+  localStorage.name = TEX_TBOX.value;
+  TEXT_BOX.value = "";
 });
 document.write(localStorage.name);
